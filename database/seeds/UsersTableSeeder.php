ada015165dbcb3aaa9752cb265fb09564a764705
zeng <shuangjiezeng@gmail.com>
2019-07-22 11:03:27 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,6 +13,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'zeng';
         $user->email = 'shuangjiezeng@gmail.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
